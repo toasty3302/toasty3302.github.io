27a4f241281cb28ec52280953287a9adad993578
Bill Nguyen <billnguyen080306@gmail.com>
2026-06-11 16:11:33 -0700

    initializing stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,27 @@
-import type { SvgComponent } from "astro/types"
-import Email from "@/assets/icons/email.svg"
-import GitHub from "@/assets/icons/github.svg"
-import RSS from "@/assets/icons/rss.svg"
-import Twitter from "@/assets/icons/twitter.svg"
+import type { SvgComponent } from "astro/types";
+import Email from "@/assets/icons/email.svg";
+import GitHub from "@/assets/icons/github.svg";
+import RSS from "@/assets/icons/rss.svg";
+import Twitter from "@/assets/icons/twitter.svg";
 
 export const SITE = {
-  title: "astro-erudite",
-  description: "An opinionated, unstyled blogging template built with Astro.",
+  title: "toasty's blogs",
+  description: "I f*ck around and I want to find out",
   locale: "en-US",
   dir: "ltr",
   defaultPageImage: "/static/opengraph-image.png",
   defaultPostImage: "/static/1200x630.png",
-} as const
+} as const;
 
 export const NAVIGATION = [
   { href: "/blog", label: "Blog" },
   { href: "/projects", label: "Projects" },
   { href: "/authors", label: "Authors" },
-]
+];
 
 export const SOCIALS: { href: string; label: string; icon: SvgComponent }[] = [
-  { href: "https://github.com/jktrn", label: "GitHub", icon: GitHub },
-  { href: "https://twitter.com/enscrbe", label: "Twitter", icon: Twitter },
-  { href: "mailto:jason@enscribe.dev", label: "Email", icon: Email },
+  { href: "https://github.com/toasty3302", label: "GitHub", icon: GitHub },
+  { href: "https://twitter.com/toasty3302", label: "Twitter", icon: Twitter },
+  { href: "mailto:billnguyen080306@gmail.com", label: "Email", icon: Email },
   { href: "/rss.xml", label: "RSS", icon: RSS },
-]
+];
